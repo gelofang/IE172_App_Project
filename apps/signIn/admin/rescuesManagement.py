d0ee7a8c5cc5b4ad96aafe90e91919dc4d54fe0b
gelofang <afang1@up.edu.ph>
2024-12-06 14:08:41 +0800

    Update rescuesManagement.py

--- apps/signIn/admin/rescuesManagement.py
+++ apps/signIn/admin/rescuesManagement.py
@@ -5,11 +5,10 @@ from dash.exceptions import PreventUpdate
 
 from app import app
 from apps.sideNavbar import adminSideNavbar as aSN
-from apps.dbconnect import getDataFromDB
+from dbconnect import getDataFromDB
 
 layout=html.Div(
     [
-        dcc.Location(id='url', refresh=False),
         aSN.adminSidebar(),
         html.Div(
             [
@@ -19,20 +18,11 @@ layout=html.Div(
                             [
                                 dbc.Col(html.H3("Rescues Management", style={"color": "#fcbf49", "margin-top": "20px"})),
                                 dbc.Col(
-                                    dbc.Button("Add", color='light', href="/rescuesManagement/add", className="home-banner-button")
+                                    dbc.Button("Add", color='light', href="/rescuesManagementProfile?mode=add", className="home-banner-button")
                                 ),
                             ], align="center"
                         ),
-                        html.Div(
-                            [
-                                html.H4('List of Rescues'),
-                                dcc.Loading(  # Wrap the output in a loading spinner
-                                    id="loading-1",
-                                    type="default",
-                                    children=[html.Div(id='rescues_rescueslist')]
-                                )
-                            ]
-                        ),
+                        html.Div('List of Rescues go here', id='rescues_rescueslist')
                     ],
                     width=9,
                     className="customer-admin-menu"
@@ -51,33 +41,28 @@ layout=html.Div(
         Input('url', 'pathname'),
     ],
 )
-
-def load_rescues_list(pathname):
+def load_rescues_list(pathname): 
+    print(pathname)
     if pathname != '/rescuesManagement':
         raise PreventUpdate
     
+    
     sql = "SELECT * FROM rescues"
     val = []
-    col = ["rescue_name","category","gender",'age','breed','med_condition',"description","rescue_id"]
+    col = ["Rescue Name","Gender",'Age','Breed','Medical Condition',"Description", "Category","rescue_id"]
 
-    try:
-        df = getDataFromDB(sql, val, col)
-        if df.empty:
-            return [html.Div("No rescues found.")]
-        
-        df['Action'] = [
+    df = getDataFromDB(sql, val, col)
+
+    df['Action'] = [
         html.Div(
             dbc.Button("Edit", color='warning', size='sm', 
-                        href = f"/rescuesManagement?mode=edit&id={row['rescue_id']}"),
+                        href = f"/rescuesManagementProfile?mode=edit&id={row['rescue_id']}"),
             className='text-center'
         ) for idx, row in df.iterrows()
         ]
-
-        df = df[["rescue_name","category","gender",'age','breed','med_condition',"description", 'Action']]
-
-        rescues_table = dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=True, size='sm')
-
-        return[rescues_table]
     
-    except Exception as e:
-        return f"Error retrieving data: {e}"
+    df = df[['Rescue Name','Gender','Age','Breed','Medical Condition','Description', 'Category', 'Action']]
+
+    rescues_table = dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=True, size='sm')
+
+    return[rescues_table]
